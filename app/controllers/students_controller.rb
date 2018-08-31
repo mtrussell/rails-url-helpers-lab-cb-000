2def9727d41cc3bda2b22c69a7ad5ac6e0db3322
Matt Trussell <smtrussell@gmail.com>
2018-08-31 19:54:43 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,7 +9,7 @@ class StudentsController < ApplicationController
     @student = set_student
     @student.active = !@student.active
     @student.save
-    redirect_to student_path(@student.id)
+    redirect_to student_path(@student)
   end
 
   def show
